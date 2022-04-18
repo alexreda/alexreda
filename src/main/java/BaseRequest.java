f3a53e38db9225736fa6b3dd6b033b33ae0425be
khassan.o.reda <khassan.o.reda@globallogic.com>
2022-04-18 23:57:19 +0200

    homework 14.08.updated

--- src/main/java/BaseRequest.java
+++ src/main/java/BaseRequest.java
@@ -1,4 +1,56 @@
-package PACKAGE_NAME;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.ErrorLoggingFilter;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.response.Response;
 
-public class BaseRequest {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Properties;
+
+import static io.restassured.RestAssured.given;
+
+class BaseRequest {
+    private static String token;
+    private static String configPath = "\\src\\main\\resources\\config.properties";
+
+    static Response login(String path, String encodedCreds, HashMap body) {
+        return given()
+                .basePath(path)
+                .header("Authorization", encodedCreds)
+                .body(body)
+                .post();
+    }
+
+    static Response GET(String path, HashMap queryParams) {
+        return given()
+                .basePath(path)
+                .header("x-token", token)
+                .queryParams(queryParams)
+                .get();
+    }
+
+    static String getToken(Response response) {
+        token = response.jsonPath().get("token");
+        return token;
+    }
+
+    static void setBaseUrl(String baseUrl) {
+        RestAssured.baseURI = baseUrl;
+    }
+
+    static void enableLogging() {
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter(), new ErrorLoggingFilter());
+    }
+
+    static String getCredentials() {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream(System.getProperty("user.dir") + configPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties.getProperty("CREDENTIALS");
+    }
 }
--- src/test/java/LoginAndFiles.java
+++ src/test/java/LoginAndFiles.java
@@ -1,56 +1,78 @@
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
 import java.util.HashMap;
 
-public class LoginAndFiles extends BaseRequest{
+class LoginAndFiles extends BaseRequest {
 
     private static Response response;
-    private static HashMap <String, Object>queryParams;
-    private static HashMap <String, Object>requestBody;
-//    public static void main(String[] args) {
-    @Test
-    public void test1(){
+    private static HashMap<String, Object> queryParams;
+    private static HashMap<String, Object> requestBody;
 
-     }
+    @BeforeAll
+    static void precondition() {
         setBaseUrl(Endpoints.BASE_URL.getLabel());
-        // test 1
-        queryParams = new HashMap<>(){{
+        enableLogging();
+    }
+
+    @Test
+    @Order(1)
+    void test1() {
+        queryParams = new HashMap<>() {{
             put("breadcrumbs", "1");
             put("offset", "0");
             put("limit", "1000");
             put("_", "1622700773180");
         }};
-        requestBody = new HashMap<>(){{
+        requestBody = new HashMap<>() {{
             put("expiry", "86400");
             put("login_from", "login page");
         }};
-        response = login( Endpoints.LOGIN_ENDPOINT.getLabel(),getCredentials(), requestBody);
-        System.out.println(getToken(response));
-         //test 2
-        response = GET(Endpoints.FILES_ROOT.getLabel(), queryParams);
+        response = login(Endpoints.LOGIN_ENDPOINT.getLabel(), getCredentials(), requestBody);
+        Assertions.assertNotNull(getToken(response));
+    }
 
-//        System.out.println(response);
-
-        //test 3
-/*        queryParams.put("folder_id", "84c966d5-8dce-429d-8f92-44d5e28b1581");
+    @Test
+    void test2() {
         response = GET(Endpoints.FILES_ROOT.getLabel(), queryParams);
-//        System.out.println(response.asString());
-        //test 4
+        Assertions.assertNotNull(response.getBody().jsonPath().get("items"));
+    }
+
+    @Test
+    void test3() {
+        queryParams.put("folder_id", "84c966d5-8dce-429d-8f92-44d5e28b1581");
+        response = GET(Endpoints.FILES_ROOT.getLabel(), queryParams);
+        Assertions.assertNotNull(response.getBody().jsonPath().get("items"));
+    }
+
+    @Test
+    void test4() {
         response = GET(Endpoints.COUNT_ENDPOINT.getLabel(), queryParams);
-//        System.out.println(response.asString());
-        // test 5
+        Assertions.assertSame(response.jsonPath().get("total"), 59);
+    }
+
+    @Test
+    void test5() {
         queryParams.put("_", "1622700773181");
         response = GET(Endpoints.RUNS.getLabel(), queryParams);
-//        System.out.println(response.asString());
-        //test 6
+        Assertions.assertTrue(response.asString().contains("runs"));
+    }
+
+    @Test
+    void test6() {
         queryParams.put("_", "1622700773184");
         queryParams.put("filter", "total");
         response = GET(Endpoints.ANALYSIS.getLabel(), queryParams);
-//        System.out.println(response.asString());
+        Assertions.assertTrue(response.asString().contains("analysis"));
+    }
 
-        //test 7
+    @Test
+    void test7() {
         queryParams.put("_", "_=1622700773185");
         response = GET(Endpoints.ARTIFACTS.getLabel(), queryParams);
-        System.out.println(response.asString());
-*/
-//    }
+        Assertions.assertTrue(response.asString().contains("artifacts"));
+    }
 }
